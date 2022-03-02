524f7e03383dab2d040a3412b241cc71585fa434
razit <razituli@gmail.com>
2022-03-02 16:22:38 +0700

    fix

--- resources/views/layouts/promocustomer/index.blade.php
+++ resources/views/layouts/promocustomer/index.blade.php
@@ -23,6 +23,7 @@
                     <th width="10%">DISKON PROMO</th>
                     <th width="15%">STATUS PROMO</th>
                     <th width="15%">EXPIRE</th>
+                    <th width="15%">SAW</th>
                 </tr>
             </thead>
             <tbody>
@@ -36,6 +37,7 @@
                     <td>Rp {{ number_format($item['promoPrice'],'0',',','.') }}</td>
                     <td>{{ $item['status'] }}</td>
                     <td>{{ $item['expired'] }}</td>
+                    <td>{{ $item['saw'] }}</td>
                 </tr>
                 @endforeach
             </tbody>
